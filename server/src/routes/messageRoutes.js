83056338e17efca4a3f728e0e5544d6e9c199e06
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-08-26 12:10:21 +0530

    Add messageRoutes to handle message retrieval with user authentication

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { protect } from "../middleware/auth.js";
+import { getMessagesWithUser } from "../controllers/messageController.js";
 
 const router = Router();
 router.get("/:userId", protect, getMessagesWithUser);
