1b41336f1c8eff41fe22da0317fb0be08d1bf1a1
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-06 15:32:15 +0530

    Fix some issues and update message controller

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -2,21 +2,30 @@ import Conversation from "../models/Conversation.js";
 import Message from "../models/Message.js";
 import mongoose from "mongoose";
 
-function participantsKey(a, b) {
-  return [a.toString(), b.toString()].sort().join(":");
-}
-
 export async function getMessagesWithUser(req, res) {
   try {
     const otherId = req.params.userId;
-    if (!mongoose.Types.ObjectId.isValid(otherId)) return res.status(400).json({ message: "Invalid user id" });
+    
+    if (!mongoose.Types.ObjectId.isValid(otherId)) {
+      return res.status(400).json({ message: "Invalid user ID" });
+    }
+
     let convo = await Conversation.findOne({
       participants: { $all: [req.user._id, otherId] }
     });
-    if (!convo) return res.json([]);
-    const msgs = await Message.find({ conversation: convo._id }).sort({ createdAt: 1 });
+
+   
+    if (!convo) {
+      return res.json([]); 
+    }
+
+    const msgs = await Message.find({ conversation: convo._id })
+      .sort({ createdAt: 1 })
+      .populate("sender", "username avatarUrl");
+
     res.json(msgs);
   } catch (e) {
-    res.status(500).json({ message: "Server error" });
+    console.error("Get messages error:", e);
+    res.status(500).json({ message: "Server error fetching messages" });
   }
 }
