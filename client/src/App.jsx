3121011bdb949146ee9bfef3b0934f8b9f39ab8a
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-15 11:29:39 +0530

    Add the  admin login route for app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register.jsx";
 import Chat from "./pages/Chat.jsx";
 import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
 import { SocketProvider } from "./context/SocketProvider.jsx";
+import AdminLogin from "./pages/AdminLogin.jsx";
 
 function PrivateRoute({ children }) {
   const { token } = useAuth();
@@ -19,6 +20,7 @@ export default function App() {
             <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/adminlogin" element={<AdminLogin/>}/>
           </Routes>
         </BrowserRouter>
       </SocketProvider>
