48dd1a7fecbb243897e549fea6d8b57b5a3536a6
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-15 21:07:25 +0530

    import Admin Dashboard and Admin Route to App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Chat from "./pages/Chat.jsx";
 import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
 import { SocketProvider } from "./context/SocketProvider.jsx";
 import AdminLogin from "./pages/AdminLogin.jsx";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminRoute from "./components/AdminRoute.jsx";
 
 function PrivateRoute({ children }) {
   const { token } = useAuth();
@@ -17,10 +19,25 @@ export default function App() {
       <SocketProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<PrivateRoute><Chat /></PrivateRoute>} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Chat />
+                </PrivateRoute>
+              }
+            />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/adminlogin" element={<AdminLogin/>}/>
+            <Route path="/adminlogin" element={<AdminLogin />} />
+            <Route
+              path="/admin/dashboard"
+              element={
+                <AdminRoute>
+                  <AdminDashboard />
+                </AdminRoute>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </SocketProvider>
