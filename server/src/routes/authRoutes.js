cfb6ed5c77bdee7d80284a07483d0541645bd8d2
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-17 22:04:30 +0530

    Update AuthController with correct paths

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { login, register, me, getUsers } from "../controllers/authController.js"; 
-import { protect } from "../middleware/auth.js";
+import { protect, adminAuth } from '../middleware/auth.js';
 
 const router = Router();
 
-router.post("/auth/register", register);
-router.post("/auth/login", login);
-router.get("/auth/me", protect, me);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, me);
 router.get("/users", protect, getUsers);
 
 export default router;
