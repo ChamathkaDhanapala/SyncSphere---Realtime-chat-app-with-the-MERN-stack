0c5f941c842de354b6a6981f3a491a9ec692d6af
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-18 19:32:33 +0530

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -44,9 +44,18 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'superadmin'],
+    default: 'user'
+  },
   registeredAt: {
     type: Date,
     default: Date.now
+  },
+  isActive: {
+    type: Boolean,
+    default: true
   },
   lastActive: {
     type: Date,
@@ -56,10 +65,16 @@ const userSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
