4dae859ce917277a76566494de40992eaa732e81
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-26 23:11:40 +0530

    Update Message model

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const messageSchema = new Schema({
-  conversation: { type: Schema.Types.ObjectId, ref: "Conversation", required: true },
-  sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+const messageSchema = new mongoose.Schema({
+  conversation: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation", required: true },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   text: { type: String, required: true },
-  seenBy: [{ type: Schema.Types.ObjectId, ref: "User" }]
+  seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
 }, { timestamps: true });
 
 export default mongoose.model("Message", messageSchema);
