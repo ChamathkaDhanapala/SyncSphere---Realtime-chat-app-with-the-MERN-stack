77e5df91c79779172f876951ba030fab58897ef5
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-26 23:12:58 +0530

    Update message routes

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { protect } from "../middleware/auth.js";
-import { getMessagesWithUser } from "../controllers/messageController.js";
+import { getMessagesWithUser, sendMessage } from "../controllers/messageController.js";
 
 const router = Router();
+
 router.get("/:userId", protect, getMessagesWithUser);
+router.post("/send", protect, sendMessage);
 
 export default router;
