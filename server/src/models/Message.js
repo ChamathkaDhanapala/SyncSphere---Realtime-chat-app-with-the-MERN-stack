c27c89cbb359bed9447e73aaeaa257690d45b9e5
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-28 22:53:49 +0530

    Add  reaction schema

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -1,10 +1,47 @@
 import mongoose from "mongoose";
 
-const messageSchema = new mongoose.Schema({
-  conversation: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation", required: true },
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  text: { type: String, required: true },
-  seenBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+const reactionSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  reaction: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
-export default mongoose.model("Message", messageSchema);
+const messageSchema = new mongoose.Schema({
+  conversation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  isFile: {
+    type: Boolean,
+    default: false
+  },
+  file: {
+    name: String,
+    url: String,
+    type: String,
+    size: Number
+  },
+  reactions: [reactionSchema],
+  seenBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, { timestamps: true });
+
+export default mongoose.model('Message', messageSchema);
