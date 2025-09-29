d75c611f08cab85366a53caa8617ec2c5ea6cdc5
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-29 15:07:03 +0530

    Add reaction Schema

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -1,47 +1,88 @@
 import mongoose from "mongoose";
 
-const reactionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const reactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reaction: {
+      type: String,
+      required: true,
+    },
   },
-  reaction: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-const messageSchema = new mongoose.Schema({
-  conversation: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Conversation',
-    required: true
-  },
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  isFile: {
-    type: Boolean,
-    default: false
-  },
-  file: {
-    name: String,
-    url: String,
-    type: String,
-    size: Number
-  },
-  reactions: [reactionSchema],
-  seenBy: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }]
-}, { timestamps: true });
+const messageSchema = new mongoose.Schema(
+  {
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    isFile: {
+      type: Boolean,
+      default: false,
+    },
+    file: {
+      name: String,
+      url: String,
+      type: String,
+      size: Number,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    deletedAt: {
+      type: Date,
+    },
+    forwardedFrom: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    isForwarded: {
+      type: Boolean,
+      default: false,
+    },
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+    pinnedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    pinnedAt: {
+      type: Date,
+    },
 
-export default mongoose.model('Message', messageSchema);
+    reactions: [reactionSchema],
+    seenBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
