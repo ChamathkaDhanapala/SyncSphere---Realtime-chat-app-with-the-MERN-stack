02f2c335bb88f99a5a492aef9281e573f16d77de
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-30 17:28:04 +0530

    Enhance conversation schema with group functionality and indexing

--- server/src/models/Conversation.js
+++ server/src/models/Conversation.js
@@ -1,15 +1,34 @@
 import mongoose from "mongoose";
 
-const conversationSchema = new mongoose.Schema({
-  participants: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  }],
-  latestMessage: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Message'
-  }
-}, { timestamps: true });
+const conversationSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    isGroup: {
+      type: Boolean,
+      default: false,
+    },
+    groupName: {
+      type: String,
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+conversationSchema.index({ participants: 1 });
+conversationSchema.index({ updatedAt: -1 });
 
 export default mongoose.model("Conversation", conversationSchema);
