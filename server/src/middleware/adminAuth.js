6173682e38a323c6f33604d12e09dfc9018df092
Chamathka Dhanapala <csd.sugandhi@gmail.com>
2025-09-30 15:03:46 +0530

    Create AdminAuth middleware

---
+++ server/src/middleware/adminAuth.js
@@ -0,0 +1,19 @@
+const adminAuth = async (req, res, next) => {
+  try {
+    // Check if user exists and is admin
+    if (req.user && req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({ 
+        message: "Access denied. Admin privileges required." 
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ 
+      message: "Server error in admin authentication", 
+      error: error.message 
+    });
+  }
+};
+
+export default adminAuth;
